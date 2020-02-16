a86f0e04a0deb13d5e412da7645bb3a72e0878af
Justin Williams <justin.scott.williams@gmail.com>
2020-02-16 14:29:52 -0600

    Stuff

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
+  items: string[] = [];
 
-  constructor() {}
+  constructor(public alertController: AlertController) {}
 
+  ngOnInit() {
+    for (let i = 0; i < 100; i++) {
+      let str: string = (Math.floor(Math.random() * 10000000) + 1).toString();
+      this.items.push(str);
+    }
+  }
+
+  async clicked(item: string) {
+    const alert = await this.alertController.create({
+      header: "Alert",
+      message: item + " clicked",
+      buttons: ["OK"]
+    });
+
+    await alert.present();
+  }
 }
